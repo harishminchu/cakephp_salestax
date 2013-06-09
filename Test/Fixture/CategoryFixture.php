2af38fb882fc9ade2255039228864ecd210f4fc4
Victor Yap <starlocke@gmail.com>
2013-06-09 15:43:32 -0400

    Updated fixtures for Category and Products

--- Test/Fixture/CategoryFixture.php
+++ Test/Fixture/CategoryFixture.php
@@ -28,9 +28,30 @@ class CategoryFixture extends CakeTestFixture {
 	public $records = array(
 		array(
 			'id' => 1,
-			'name' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+			'name' => 'books',
+			'taxable' => 0
+		),
+		array(
+			'id' => 2,
+			'name' => 'food',
+			'taxable' => 0
+		),
+		array(
+			'id' => 3,
+			'name' => 'medical products',
+			'taxable' => 0
+		),
+		array(
+			'id' => 4,
+			'name' => 'music',
 			'taxable' => 1
 		),
+		array(
+			'id' => 5,
+			'name' => 'beauty products',
+			'taxable' => 1
+		),
+
 	);
 
 }
